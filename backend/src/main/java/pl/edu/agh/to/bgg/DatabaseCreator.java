6a990e08a68cf17dbee25b7012a00d976d550885
KusKus <maciekkus16@gmail.com>
2025-12-09 19:54:02 +0100

    changed foldername

--- backend/src/main/java/pl/edu/agh/to/bgg/DatabaseCreator.java
+++ backend/src/main/java/pl/edu/agh/to/bgg/DatabaseCreator.java
@@ -4,15 +4,19 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import pl.edu.agh.to.bgg.boardgame.BoardGame;
 import pl.edu.agh.to.bgg.boardgame.BoardGameRepository;
+import pl.edu.agh.to.bgg.user.User;
+import pl.edu.agh.to.bgg.user.UserRepository;
 
 import java.util.List;
 
 @Configuration
 public class DatabaseCreator {
     private final BoardGameRepository boardGameRepository;
+    private final UserRepository userRepository;
 
-    public DatabaseCreator(BoardGameRepository boardGameRepository) {
+    public DatabaseCreator(BoardGameRepository boardGameRepository, UserRepository userRepository) {
         this.boardGameRepository = boardGameRepository;
+        this.userRepository = userRepository;
     }
 
     @PostConstruct
@@ -32,7 +36,10 @@ public class DatabaseCreator {
                 4,
                 90
         );
+        
+        User u = new User("mag");
 
         boardGameRepository.saveAll(List.of(bg1, bg2));
+        userRepository.saveAll(List.of(u));
     }
 }
