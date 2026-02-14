1e58a793f6748dda442ec4aefe9e6baba2ab6316
KusKus <maciekkus16@gmail.com>
2026-02-14 21:18:16 +0100

    added docker compose

--- backend/src/main/java/pl/edu/agh/to/bgg/configs/CorsConfig.java
+++ backend/src/main/java/pl/edu/agh/to/bgg/configs/CorsConfig.java
@@ -1,14 +1,18 @@
 package pl.edu.agh.to.bgg.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
 public class CorsConfig {
+    private final String frontendURL;
+
+    public CorsConfig(@Value("${app.frontend-url}") String frontendURL) {
+        this.frontendURL = frontendURL;
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -17,7 +21,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendURL)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
